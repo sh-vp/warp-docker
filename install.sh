0cfda22604e02c7e1a180670147a85fb2d31e94f
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-10-23 19:32:31 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+red='\033[1;91m'          # Red
+green='\033[1;92m'        # Green
+yellow='\033[1;93m'       # Yellow
+White='\033[1;97m'        # White
+blue='\033[1;94m'         # blue
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
+
+# Check OS and set release variable
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    release=$ID
+elif [[ -f /usr/lib/os-release ]]; then
+    source /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+echo "The OS release is: $release"
+
+    case "${release}" in
+    centos | almalinux | rocky)
+        yum -y update && yum install docker docker-compose -y -q
+        ;;
+    *)
+    apt-get update && apt-get install docker docker-compose -y -q
+        ;;
+    esac
+  mkdir /etc/warp
+  wget --no-check-certificate -O /etc/warp/docker-compose.yml https://raw.githubusercontent.com/sh-vp/warp-docker/main/docker-compose.yml
+  cd /etc/warp && docker-compose up -d
+  clear
+echo -e "${yellow} _       __                    _____           _       __ "
+echo -e "${yellow}| |     / /___ __________     / ___/__________(_)___  / /_"
+echo -e "${yellow}| | /| / / __ `/ ___/ __ \    \__ \/ ___/ ___/ / __ \/ __/"
+echo -e "${yellow}| |/ |/ / /_/ / /  / /_/ /   ___/ / /__/ /  / / /_/ / /_  "
+echo -e "${yellow}|__/|__/\__,_/_/  / .___/   /____/\___/_/  /_/ .___/\__/  "
+echo -e "${yellow}                 /_/                        /_/           "
+echo -e ""
+echo -e "${red}============================================"
+echo -e ""
+echo -e "${yellow}  https://github.com/sh-vp/Warp-Docker"
+echo -e ""
+echo -e "${green}  Author: Shadow-dev  "
+echo -e ""
+echo -e "${red}============================================"
+echo -e ""
+echo -e "${yellow}Warp Service${green} Activated Successfully! ${White}"
+echo -e ""
+echo -e ""
+echo -e ""
